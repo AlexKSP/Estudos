8c5aad5b023ea54bc8dcca9c6a872976a40fcde7
AlexKSP <127268092+AlexKSP@users.noreply.github.com>
2023-12-24 20:08:48 -0300

    Desafio JS 2, numeros formatados

--- Js-modulo 1/conversor-de-moeda-correção/script.js
+++ Js-modulo 1/conversor-de-moeda-correção/script.js
@@ -3,13 +3,24 @@ const convertButton = document.querySelector("#convert-button")
 
 function convertValues () {
     const inputCurrencyValue = document.querySelector("#input-value").value
+    
+    const currencyValueToCovnert = document.querySelector(".currency-value-to-convert")
+    const currencyValueConverted = document.querySelector(".currency-value")
 
     const dolarToday = 4.86
 
     const convertedValue = inputCurrencyValue / dolarToday
 
+    currencyValueToCovnert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)//quem eu quero formatar
 
-    console.log(convertedValue)
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+    }).format(convertedValue)
+
 }
 
 convertButton.addEventListener("click", convertValues)
