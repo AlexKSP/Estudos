edbbe3aee22ae08b4959b5ace15a77652279eaf9
AlexKSP <127268092+AlexKSP@users.noreply.github.com>
2023-12-24 20:46:59 -0300

    Finalizado desafio.

--- Js-modulo 1/conversor-de-moeda-correção/script.js
+++ Js-modulo 1/conversor-de-moeda-correção/script.js
@@ -1,26 +1,56 @@
 const convertButton = document.querySelector("#convert-button")
+const currencySelect = document.querySelector(".currency-select")
 
 
 function convertValues () {
     const inputCurrencyValue = document.querySelector("#input-value").value
-    
+
     const currencyValueToCovnert = document.querySelector(".currency-value-to-convert")
     const currencyValueConverted = document.querySelector(".currency-value")
 
+    
     const dolarToday = 4.86
+    const euroToday = 5.35 
 
-    const convertedValue = inputCurrencyValue / dolarToday
+    if (currencySelect.value == "dolar") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+    }
+
+    if (currencySelect.value == "euro") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue/euroToday)
+    }
 
     currencyValueToCovnert.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL"
     }).format(inputCurrencyValue)//quem eu quero formatar
 
-    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD"
-    }).format(convertedValue)
+
 
 }
 
+function changeCurrency() {
+    const currencyImg = document.querySelector(".logo-coin-converted")
+    const currencyName = document.getElementById("currency-name")
+
+    if (currencySelect.value == "dolar") {
+        currencyName.innerHTML = "Dólar americano"
+        currencyImg.src = "./assets/usa-icon.png"
+    }
+    
+    if (currencySelect.value == "euro") {
+        currencyName.innerHTML = "Euro"
+        currencyImg.src = "./assets/euro-icon.png"
+    }
+
+    convertValues()
+}
+
 convertButton.addEventListener("click", convertValues)
+currencySelect.addEventListener("change", changeCurrency)
