9ae5513c614ab8b68c99b97cc7f412816d77c40d
AlexKSP <lex.ksp@gmail.com>
2023-12-31 13:12:01 -0300

    alteração no js com o switch

--- Projetos/Interactive-rating-component/script.js
+++ Projetos/Interactive-rating-component/script.js
@@ -5,13 +5,39 @@
 // const rating5 = document.getElementById('5').value
 
 const submit = document.getElementById('submit')
+const container = document.querySelector('.container')
 
 function click() {
     const radio = document.querySelector('input[name="radio"]:checked').value
 
+    switch (radio) {
+        case 1:
+            container.style.backgroundColor = '#FB7413'
+            console.log('1')
+            break;
+        case 2:
+            container.style.backgroundColor = '#FB7413'
+            console.log('2')
+            break;
+        case 3:
+            container.style.backgroundColor = '#FB7413'
+            console.log('3')
+            break;
+        case 4:
+            container.style.backgroundColor = '#FB7413'
+            console.log('4')
+            break;        
+        
+        default:
+            container.style.backgroundColor = '#FB7413'
+            console.log('5')
+            break;
+    }
+
+    
     
 
-    console.log(radio)
+    //console.log(radio)
 }
 
 submit.addEventListener('click', click)
