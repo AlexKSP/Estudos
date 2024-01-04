28bf8594e00c6f1cc68f53511910a54c60e92c39
AlexKSP <lex.ksp@gmail.com>
2024-01-04 00:27:08 -0300

    correção exercicio 7 de js

--- Js-modulo-2/7-carrinhho-exercicio.js
+++ Js-modulo-2/7-carrinhho-exercicio.js
@@ -1,57 +1,20 @@
-const products = [
-    {
-        name: 'Marcarrão',
-        price: 10
-    },
-    {
-        name: 'Molho de tomate',
-        price: 5
-    },
-    {
-        name: 'Carne moida (Kg)',
-        price: 33
-    },
-    {
-        name: 'Queijo (Kg)',
-        price: 90
-    },
-    {
-        name: 'Coca-cola 3L',
-        price: 12
-    }
-]
-    
-function discount() {
-    const product = products
-    console.log(product)
-    let i = 0
-    let myArray = []
-    while (i < product.length) {
-            
-        //Desconto sendo aplicado em cada item do array acima de 30$
-        myArray[i] = product[i].price
-        if (product[i].price > 30) {
-            product[i].price = product[i].price - product[i].price / 10
-            myArray[i] = product[i].price
-        }
-        i++
-    }
-    return myArray
+const cart = [10, 244, 100, 2, 30, 25, 250]
+let finalValueWithDiscount = 0
+
+function calculateDiscount(price, discount){
+    const result = (price * discount) / 100
+    return result
 }
 
-function subTotal() {
-    let total = 0
-    for (let i=0; i < products.length ;i++) {
-        total+=products[i].price // total = total + preço do produto na posição i
-        console.log(total + ' - ' + i )
+cart.forEach((value) => {
+
+    if (value > 30){
+        const discount = calculateDiscount(value, 10)
+        finalValueWithDiscount += (value - discount)
+    } else {
+        finalValueWithDiscount += value
     }
-    return total
-}
 
-console.log(products[2].price)
-const cart = discount()
-const total = subTotal()
-console.log(products[2].price)
-//console.log(total + ' : ' + cart)
-
-
+});
+//arrume o console log de forma formal informando ao cliente, o valor final, o valor com desconto, e quanto ele economizou. toFixed(2)
+console.log(finalValueWithDiscount)
